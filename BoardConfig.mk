e2e77e343849550f65b2fb47e440214ce914f45c
krabappel2548 <krabappel2548@gmail.com>
2014-11-16 10:07:31 +0100

    Move common things to new msm8974_common repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include device/sony/msm8974_common/BoardConfigCommon.mk
+
 PRODUCT_VENDOR_KERNEL_HEADERS := device/sony/shinano/kernel-headers
 
 TARGET_ARCH := arm
