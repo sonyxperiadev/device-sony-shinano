f7f7c401217c37ec4540a179b9bc7b152c922464
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-01-12 14:47:12 +0100

    OMX: enable build from source
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- device.mk
+++ device.mk
@@ -90,6 +90,16 @@ PRODUCT_PACKAGES += \
     libqdutils \
     libqdMetaData
 
+#OMX
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libstagefrighthw \
+    libOmxCore \
+    libmm-omxcore \
+    libOmxVdec \
+    libOmxVdecHevc \
+    libOmxVenc
+
 PRODUCT_PACKAGES += \
     lights.shinano
 
