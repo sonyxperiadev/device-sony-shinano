c82a82d34794659ac03eb0fb68697ae354dd80a9
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-02-02 14:33:37 +0100

    Revert "bcmdhd: use sony fw for now"
    there is no need to duplicate those files
    
    This reverts commit 70e24a483367096f97a4e0643a45205e5f60b28c.

--- device.mk
+++ device.mk
@@ -28,8 +28,6 @@ PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/system/usr/idc/max1187x_touchscreen_0.idc:system/usr/idc/max1187x_touchscreen_0.idc \
     $(SONY_ROOT)/system/usr/idc/clearpad.idc:system/usr/idc/clearpad.idc \
     $(SONY_ROOT)/system/etc/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf \
-    $(SONY_ROOT)/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(SONY_ROOT)/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(SONY_ROOT)/system/etc/sec_config:system/etc/sec_config \
     $(SONY_ROOT)/system/etc/gps.conf:system/etc/gps.conf
 
