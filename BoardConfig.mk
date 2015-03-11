f8b8d6da4524b5715eb42c0f60c67a8297a3707d
galaxyfreak <marinspajic17@hotmail.com>
2015-03-11 14:38:27 +0100

    Remove BOARD_USES_GENERIC_AUDIO
    BOARD_USES_GENERIC_AUDIO is obsolete https://android.googlesource.com/platform/hardware/libhardware_legacy/+/android-5.0.2_r1/audio/Android.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,6 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 BOARD_EGL_CFG := device/sony/shinano/rootdir/system/lib/egl/egl.cfg
 
-#BOARD_USES_GENERIC_AUDIO := false
 BOARD_USES_ALSA_AUDIO := true
 
 TARGET_USES_ION := true
