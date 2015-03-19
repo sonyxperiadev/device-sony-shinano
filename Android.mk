6a528a84bea58068efa212d5179e5e5bbe434afe
Chirayu Desai <chirayudesai1@gmail.com>
2015-03-19 21:50:24 +0530

    Fix copying of multiple dtb files

--- Android.mk
+++ Android.mk
@@ -5,10 +5,12 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BUILD_KERNEL),)
 INSTALLED_KERNEL_TARGET ?= $(PRODUCT_OUT)/kernel
 
-DTB_FILES := $(shell find -L $(ANDROID_BUILD_TOP)/device/sony/$(TARGET_DEVICE)/dtbs -name "*.dtb")
-DTBS := $(foreach dtb,$(DTB_FILES),$(PRODUCT_OUT)/dtbs/$(notdir $(dtb)))
-$(DTBS) : $(DTB_FILES) | $(ACP)
-	$(hide) @mkdir -p $(PRODUCT_OUT)/dtbs
+DTB_DIR := $(ANDROID_BUILD_TOP)/device/sony/$(TARGET_DEVICE)/dtbs
+DTB_FILES := $(shell find -L $(DTB_DIR) -name "*.dtb")
+DTB_OUT_DIR := $(PRODUCT_OUT)/dtbs
+DTBS := $(foreach dtb,$(DTB_FILES),$(DTB_OUT_DIR)/$(notdir $(dtb)))
+$(DTB_OUT_DIR)/%.dtb : $(DTB_DIR)/%.dtb | $(ACP)
+	$(hide) @mkdir -p $(DTB_OUT_DIR)
 	$(transform-prebuilt-to-target)
 
 file := $(INSTALLED_KERNEL_TARGET)
