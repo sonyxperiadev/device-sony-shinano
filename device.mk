850f00fadd9d1fb79c4b4105106730bc50e80c6f
Adam Farden <adam@farden.cz>
2015-05-23 13:12:46 +0200

    shinano: qmi: stop the missing modem logspam
    The 'error' messages are telling us we don't have an external radio,
    but of course we have an integrated radio, so networking works fine.
    
    The fix would be to modify the proprietary QMI code, but it's easier
    to simply hide it.
    
    Signed-off-by: Adam Farden <adam@farden.cz>

--- device.mk
+++ device.mk
@@ -200,5 +200,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Platform specific default properties
 #
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
-
+    persist.sys.usb.config=mtp \
+    persist.data.qmi.adb_logmask=0
