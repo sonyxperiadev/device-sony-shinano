8005a8b97bd45aba38d4eb5a87696d585ec2a29f
relan <relan@users.noreply.github.com>
2015-06-22 22:52:08 +0300

    castor_windy, scorpion_windy: fix missing libaudioamp and liblights
    The condition filtered out castor_windy and scorpion_windy and thus
    libaudioamp and liblights were not included. This resulted in non-working
    brightness control.

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(filter-out aries castor leo scorpion sirius,$(TARGET_DEVICE)),)
+ifeq ($(filter-out aries castor castor_windy leo scorpion scorpion_windy sirius,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
