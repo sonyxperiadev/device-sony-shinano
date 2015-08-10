dae67dd84fed82d081b2175ff38a32aebfb9ca36
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-08-10 19:15:49 +0200

    rename boringssl-fixer-upper
    use consistent naming
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- device.mk
+++ device.mk
@@ -198,7 +198,7 @@ PRODUCT_PACKAGES += \
 
 # BoringSSL hacks
 PRODUCT_PACKAGES += \
-    libboringssl-fixer-upper
+    libboringssl-compat
 
 # APN list
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
