5684f5c35c0a2074ae93b148051be71b06e6cfc7
Humberto Borba <humberos@gmail.com>
2015-10-29 13:02:37 -0200

    shinano: Move audio policy to device
    EARPIECE is not preset on Tablets.
    So move common audio policy to device.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- device.mk
+++ device.mk
@@ -58,7 +58,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/system/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(SONY_ROOT)/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
     $(SONY_ROOT)/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
     $(SONY_ROOT)/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
     $(SONY_ROOT)/system/etc/audio_platform_info.xml:system/etc/audio_platform_info.xml \
