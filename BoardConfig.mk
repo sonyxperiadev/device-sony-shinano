01214621b971a043abde293a5a128f2443a5aefb
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-01-10 10:45:42 +0100

    Include build helpers for QCOM proprietary
    import flags to build qcom proprietary sources
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,9 @@ endif
 BUILD_KERNEL := true
 -include vendor/sony/kernel/KernelConfig.mk
 
+# Include build helpers for QCOM proprietary
+-include vendor/qcom/proprietary/common/build/proprietary-build.mk
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
