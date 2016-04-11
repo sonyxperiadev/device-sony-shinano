1121f835a674876b9b2a9efdb94c0007551435e5
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-04-11 14:41:57 +0200

    add sensor configuration
    disable sensor probe and configure sensors according to platform
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- platform.mk
+++ platform.mk
@@ -93,3 +93,37 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # report major/minor versions as 3
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
+
+# aDSP sensors
+## max rate
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qti.sensors.max_accel_rate=false \
+    ro.qti.sensors.max_gyro_rate=false \
+    ro.qti.sensors.max_mag_rate=false \
+    ro.qti.sensors.max_geomag_rotv=50
+
+## sensor type
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qti.sdk.sensors.gestures=false \
+    ro.qti.sensors.pedometer=false \
+    ro.qti.sensors.step_detector=false \
+    ro.qti.sensors.step_counter=false \
+    ro.qti.sensors.pam=false \
+    ro.qti.sensors.scrn_ortn=false \
+    ro.qti.sensors.smd=true \
+    ro.qti.sensors.game_rv=false \
+    ro.qti.sensors.georv=false \
+    ro.qti.sensors.cmc=false \
+    ro.qti.sensors.bte=false \
+    ro.qti.sensors.fns=false \
+    ro.qti.sensors.qmd=false \
+    ro.qti.sensors.amd=false \
+    ro.qti.sensors.rmd=false \
+    ro.qti.sensors.vmd=false \
+    ro.qti.sensors.gtap=false \
+    ro.qti.sensors.tap=false \
+    ro.qti.sensors.facing=false \
+    ro.qti.sensors.tilt=false \
+    ro.qti.sensors.tilt_detector=false \
+    ro.qti.sensors.dpc=false \
+    ro.qti.sensors.wu=false
