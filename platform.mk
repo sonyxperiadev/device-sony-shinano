00e923157504c30b2edeffcfe7648606bc2f1bd1
David <Davidteri91@gmail.com>
2017-01-18 10:20:35 +0100

    shinano: remove nfc host card emulation
    it is causing fail connection between aosp devices (pn547 - pn547) (pn547 - pn544)
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- platform.mk
+++ platform.mk
@@ -46,7 +46,6 @@ PRODUCT_COPY_FILES += \
 # Device Specific Hardware
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 # Platform Init
