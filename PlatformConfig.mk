30896b19ca7cc17a7d1e7d331eed1dce627104fd
David <Davidteri91@gmail.com>
2017-02-15 23:17:18 +0100

    Revert "shinano: enable legacy camera fixes"
    This reverts commit e718b8994b909c1de032773785abf5303619bf79.
    
    This was moved to common

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -46,9 +46,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
 TARGET_RECOVERY_FSTAB = $(PLATFORM_COMMON_PATH)/rootdir/fstab.shinano
 
-# Camera HAL1 hack on 7.x
-TARGET_HAS_LEGACY_CAMERA_HAL1 := true
-
 # Wi-Fi definitions for Broadcom solution
 BOARD_WLAN_DEVICE           := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
