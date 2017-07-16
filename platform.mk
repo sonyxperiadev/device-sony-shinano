0490439d1f3fd9f70887467a856a4ef46a536453
Christian Oder <myself5@carbonrom.org>
2017-07-16 14:46:15 +0200

    shinano: Add MiFare permission file

--- platform.mk
+++ platform.mk
@@ -50,7 +50,8 @@ PRODUCT_COPY_FILES += \
 # Device Specific Hardware
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
 # Platform Init
 PRODUCT_PACKAGES += \
