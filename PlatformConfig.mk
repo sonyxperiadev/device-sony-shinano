01b5b9cd9e3da7973ae2faadd69ad11ff9da75f5
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-07-29 11:50:46 +0200

    declare NXP_CHIP_FW_TYPE for firmware selection
    since the NXP_CHIP_TYPE is changed by a new android release
    we declare our NXP_CHIP_FW_TYPE to select proper firmware
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -68,6 +68,7 @@ BOARD_CUSTOM_BT_CONFIG := $(PLATFORM_COMMON_PATH)/bluetooth/vnd_generic.txt
 
 # NFC
 NXP_CHIP_TYPE := PN547C2
+NXP_CHIP_FW_TYPE := PN547C2
 
 # FM definitions for Broadcom solution
 BOARD_HAVE_ALTERNATE_FM := true
