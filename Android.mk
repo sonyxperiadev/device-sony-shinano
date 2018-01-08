412dd2a47b9613f38b368e459ef49730cbbc1e63
Alin Jerpelea <alin.jerpelea@sony.com>
2018-01-08 13:16:33 +0100

    Revert "add legacy NFC symlink"
    This reverts commit dd63266ff60a96f38144b5f245a1a041e08048ba.

--- Android.mk
+++ Android.mk
@@ -1,8 +1,5 @@
 ifeq (shinano,$(PRODUCT_PLATFORM))
 
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/firmware)
-$(shell pushd $(PRODUCT_OUT)/system/vendor/firmware > /dev/null && ln -s /vendor/etc/firmware/libpn547_fw.so libpn547_fw.so && popd > /dev/null)
-
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
